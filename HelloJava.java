7aaac9e4fb5e57450d5ed682c0a4ba14ec286937
LabFnP <servicelfp2018@gmail.com>
2021-02-18 16:42:14 +0800

    first

---
+++ HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava
+{
+   public static void main(String[] args)
+   {int[] a = new int[10];
+    
+   for (int i = 0; i < 10; i++)
+       a[i] = 9 - i;
+          
+   for (int i = 0; i < 10; i++)
+      a[i] = a[a[i]];
+   System.out.println(a[5]);
+   }
+}
